2bcfbf711a26388ba93368204849f07b4f714cb4
Hiren Jadav <h.r.jadav252@gmail.com>
2024-10-10 10:22:18 +0530

    added side bar

--- src/assets/fontawesome.ts
+++ src/assets/fontawesome.ts
@@ -1,5 +1,5 @@
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faArrowRight, faCalendarDays, faChevronLeft, faCircleInfo, faClipboard, faEnvelope, faFaceSmile, faKey, faMagnifyingGlass, faMinus, faPaperclip, faPencil, faPlus, faSackDollar, faSpinner, faTrash, faUser } from "@fortawesome/free-solid-svg-icons";
+import { faArrowRight, faCalendarDays, faChevronLeft, faCircleInfo, faClipboard, faEnvelope, faFaceSmile, faKey, faMagnifyingGlass, faMinus, faPaperclip, faPencil, faPlus, faSackDollar, faSpinner, faTrash, faUser, faUserGroup } from "@fortawesome/free-solid-svg-icons";
 import { faGem } from "@fortawesome/free-regular-svg-icons";
 
-library.add(faUser, faMagnifyingGlass, faSpinner, faPlus, faMinus, faClipboard, faCalendarDays, faSackDollar, faChevronLeft, faTrash, faPencil, faKey, faGem, faEnvelope, faPaperclip, faFaceSmile, faArrowRight, faCircleInfo);
+library.add(faUser, faMagnifyingGlass, faSpinner, faPlus, faMinus, faClipboard, faCalendarDays, faSackDollar, faChevronLeft, faTrash, faPencil, faKey, faGem, faEnvelope, faPaperclip, faFaceSmile, faArrowRight, faCircleInfo, faUserGroup);
--- src/pages/ChatHomePage/ChatHomePage.tsx
+++ src/pages/ChatHomePage/ChatHomePage.tsx
@@ -2,10 +2,25 @@ import React from "react";
 import "./ChatHomePage.scss";
 import ChatList from "../../components/ChatList/ChatList";
 import SingleChat from "../../components/SingleChat/SingleChat";
+import { Button } from "primereact/button";
+import FontIconWrapper from "../../components/FontIconWrapper";
 
 export default function ChatHomePage() {
   return (
-    <div className="min-vh-100 h-100 p-3 d-flex">
+    <div className="min-vh-100 h-100 d-flex">
+      <div className="chat-sidebar-container">
+        <Button text className="sidebar-button" tooltip="Chats">
+          <FontIconWrapper icon="fa-solid fa-user-group" />
+        </Button>
+
+        <Button text className="sidebar-button" tooltip="Groups">
+          <FontIconWrapper icon="fa-solid fa-user-group" />
+        </Button>
+
+        <Button text className="sidebar-button" tooltip="Channels">
+          <FontIconWrapper icon="fa-solid fa-user-group" />
+        </Button>
+      </div>
       <div className="chat-home-container">
         <div className="chat-list">
           <div>
